adcc1ab1532e223229b37c7974da7dde3a3df049
Jake Lawrence <jake.lawrence24@gmail.com>
2017-02-15 16:52:22 -0500

    finished email

--- blog_ws.py
+++ blog_ws.py
@@ -3,6 +3,7 @@ import urllib
 import matplotlib.pyplot as plt
 import threading
 import smtplib
+import os
 
 cached_posts = []
 
@@ -17,11 +18,24 @@ def find_new_posts(_cached_posts=cached_posts):
 
     cant_send = not(_cached_posts)
 
+    # initialization of email server
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    # initialization of the mail driver
+    mail.ehlo()
+    mail.starttls()
+
+    senderEmail = ''
+    senderPass = ''
+
+    recieverEmail = 'jake.lawrence24@gmail.com'
+
+    mail.login(senderEmail, senderPass)
+
     for post in posts:
         if(not(post in _cached_posts)):
             _cached_posts.append(post)
             if(not(cant_send)):
-                print('New post added:' + post.text)
+                mail.sendmail(senderEmail,recieverEmail,'New post at jlawrence.co/tb')
     threading.Timer(t, find_new_posts).start()
 
 find_new_posts()
